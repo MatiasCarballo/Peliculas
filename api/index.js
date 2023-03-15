5fd9a7beac056bd2dda40cf985dd991cb950fcd3
MatiasCarballo <matiascarballo03@hotmail.com>
2023-03-15 19:01:06 -0300

    carpetas y demas

--- api/index.js
+++ api/index.js
@@ -1,10 +1,7 @@
 const app = require('./src/app.js')
 require('dotenv').config()
-const {
-    PORT
-  } = process.env;  
+const port = process.env.PORT || 3005;
 
-const port = 3001;  
 
 app.listen(port, ()=> {
     console.log(`Server corriendo en el puerto ${port}`);
