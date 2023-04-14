77c015e828f56923e54474b938058327f4407a94
Luis Cera Sanjuan <90820095+lfceras@users.noreply.github.com>
2023-04-14 15:26:32 -0500

    Luis (#4)
    * end point usables
    
    * a
    
    * agregando redux
    
    ---------
    
    Co-authored-by: MatiasCarballo <matiascarballo03@hotmail.com>

--- api/src/Model/Prueba.js
+++ api/src/Model/Prueba.js
@@ -11,7 +11,8 @@ const pruebaSchema = new Schema({
     boolean:{
         type: Boolean
     }
-}, { toJSON: { transform: (doc, ret) => { ret.id = ret._id.toString(); delete ret._id; delete ret.__v; } } });
+});
 
 const Prueba = model('Prueba', pruebaSchema);
 module.exports = Prueba;
+
