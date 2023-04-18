ccca6cf69637f9cbb26c594bb67e024d763f4db4
tomiw123 <tomacowalker@gmail.com>
2023-04-18 16:35:51 -0300

    footer!

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,51 +1,39 @@
 import React from "react";
-// import { Link } from "react-router-dom";
-// import { FaFacebookF, FaInstagram, FaWhatsapp } from "react-icons/fa";
+//import { Link } from "react-router-dom";
+import { FaFacebookF, FaInstagram, FaWhatsapp } from "react-icons/fa";
 import "./Footer.css";
+import image from "../../assets/luisfooter.jpeg"
 
 
 const Footer = () => {
     return (
-      <div className="footer">
-        <div className="saltooooo"></div>
-  
-        <div className="contact">
-          <a href="" target="_blanc">
-            <div className="rs">
-              {/* <FaFacebookF className="icon" /> */}
-            </div>
-          </a>
-          <a
-            href=""
-            target="_blanc"
-          >
-            <div className="rs">
-              {/* <FaInstagram className="icon" /> */}
-            </div>
-          </a>
-  
-          <a
-            href=""
-            target="_blanc"
-          >
-            <div className="rs">
-              {/* <FaWhatsapp className="icon" style={{ width: "80px" }} /> */}
-            </div>
-          </a>
-          <div className="logo">
-            {/* <Link to="/">
-              <img src={image} alt="not-found" width={200} />
-            </Link> */}
+      <footer className="pie-pagina">
+        <div className="grupo-1">
+          <div className="box">
+            <figure>
+              <a href="#">
+                <img src={image} alt="logo pagina" />
+              </a>
+            </figure>
           </div>
-          <div className="teldic">
-            {/* <Link to={"/contacto"}>
-              <h1>CONTACTO</h1>
-            </Link> */}
-            <h2>Email: </h2>
-            <h2>Telefono:</h2>
+          <div className="box">
+            <h2>SOBRE LUISITO-COMUNICA</h2>
+            <p>Un Chikes porque todavia no se le sabe el genero, buen pibe!</p>
+            <p>Solamente dice que esta durmiendo la siesta mientra se esta viendo el partido del real madrid</p>
+          </div>
+          <div className="box">
+          <h2>SIGUENOS</h2>
+          <div className="red-social">
+            <FaFacebookF className="icon"></FaFacebookF>
+            <FaInstagram className="icon"></FaInstagram>
+            <FaWhatsapp className="icon"></FaWhatsapp>
+          </div>
           </div>
         </div>
-      </div>
+        <div className="grupo-2">
+          <small>&copy; 2023 <b>Nombre pagina</b> - Todos los Derechos Reservados. </small>
+        </div>
+      </footer>
     );
   };
   
